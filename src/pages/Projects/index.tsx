6c4d7de580f6186fc10ff39a449e806a3265d110
vbuarque <viniciusbuarque17@gmail.com>
2022-11-14 21:06:04 -0300

    Adicionando a tela de projetos feitos

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -19,10 +19,6 @@ export function Projects() {
 
             <CardItem title='Teste' information='Help Desk desenvolvido para gerenciar o ciclo de vida de um chamado, aplicando segurança com autorização e autenticação JWT ' image=''/>
 
-            <CardItem title='Teste' information='Help Desk desenvolvido para gerenciar o ciclo de vida de um chamado, aplicando segurança com autorização e autenticação JWT ' image=''/>
-
-            <CardItem title='Teste' information='Help Desk desenvolvido para gerenciar o ciclo de vida de um chamado, aplicando segurança com autorização e autenticação JWT ' image=''/>
-
         </ContainerCards>
     </Container>
   );
--- src/pages/Projects/styles.ts
+++ src/pages/Projects/styles.ts
@@ -16,6 +16,14 @@ export const HeadTitle = styled.div`
 export const Title = styled.h1`
     font-size: 3rem;
     color: ${props => props.theme.colors.text};
+
+    @media (max-width: 560px) {
+        font-size: 2rem;
+    }
+
+    @media (max-width: 435px) {
+        font-size: 1.5rem;
+    }
 `;
 
 export const HeadSubtitle = styled.div`
@@ -29,6 +37,14 @@ export const Subtitle = styled.h2`
     color: ${props => props.theme.colors.text};
     font-weight: 400;
     font-size: 2rem;
+
+    @media (max-width: 560px) {
+        font-size: 1rem;
+    }
+
+    @media (max-width: 320px) {
+        font-size: 12px;
+    }
 `;
 
 export const ContainerCards = styled.div`
